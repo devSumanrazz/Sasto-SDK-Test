430b6940dc7219ede2c021e719d127eefaa2510e
Suman Raj Pathak <viper.suman@gmail.com>
2022-12-07 12:14:47 +0545

    initial commit

---
+++ app/src/main/java/com/ndpc/sastosdktest/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.ndpc.sastosdktest
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.ndpc.sastosdktest.databinding.ActivityMainBinding
+import com.sastotickets.android.sdk.utils.SastoTicketsConfiguration
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        initSDK()
+    }
+
+    private fun initSDK() {
+        SastoTicketsConfiguration().initilizeSDK(this, "ndpc@sastotickets.com", "hzG4D-kj",
+            "9860925642",
+            100.0,callback =({response ,errorMessage->
+                if (response != null) {
+                    println("--------------")
+                    println(response)
+                    println("--------------")
+                    Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Log.e("sastoticketdebugerror ", "callback" + errorMessage.toString())
+
+                }
+            }))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
